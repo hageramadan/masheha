5389a3f8b48f9d91487c1683d6035ee84bb3a74b
hagerramadan92 <hagerramadan@renix4tech.com>
2026-08-13 16:41:57 +0300

    add loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 "use client";
+import { Suspense } from 'react';
 import FeaturedCars from "@/src/components/home/FeaturedCars";
 import HeroSliderSwiper from "@/src/components/home/HeroSliderSwiper";
 import DownloadSection from "@/src/components/home/downloadSection";
+import LoadingScreen from "@/src/components/common/LoadingScreen";
+
 export default function Home() {
   return (
-    <>
+    <LoadingScreen>
       <HeroSliderSwiper />
-
       <FeaturedCars />
-
       <DownloadSection />
-    </>
+    </LoadingScreen>
   );
 }
